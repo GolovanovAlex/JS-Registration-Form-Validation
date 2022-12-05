064215a59443c7961cf35fd9bc4f8978b21468b3
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-12-06 02:25:29 +0300

    js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ let userNameError = document.getElementById('user_name_error');
 let userNameEmpty = document.getElementById('user_name_empty');
 
 let emailInput = document.getElementById('email_input');
-let emailEttor = document.getElementById('email_error');
+let emailError = document.getElementById('email_error');
 let emailEmpty = document.getElementById('email_empty');
 
 let phoneInput = document.getElementById('phone_input');
@@ -57,3 +57,174 @@ let regiterBtn = document.getElementById('submit_btn');
 
 let validCls = document.getElementById('valid');
 let invalidCls = document.getElementById('error');
+
+// ---- ---- Verifycation ---- ---- //
+const textVerify = (text) => {
+  const regex = /^[a-zA-Z]{3,}$/;
+  return regex.test(text);
+};
+
+const emailVerify = (input) => {
+  const regex = /^[a-z0-9_]+@[a-z]{3,}\.[a-z\.]{3,}$/;
+  return regex.test(input);
+};
+
+const phoneVerify = (number) => {
+  const regex = /^[0-9]{10}$/;
+  return regex.test(number);
+};
+
+const passwordVerify = (password) => {
+  const regex =
+    /^(?=.+[a-z])(?=.+[A-Z])(?=.+[0-9])(?=.+[\$\%\^\&\!@\#\*\(\)\+\=\?\>\<])/;
+  return regex.test(password) && password.length >= 8;
+};
+
+// ---- ---- Password Validation ---- ---- //
+let vallowChar = document.getElementById('lowChar');
+let valupChar = document.getElementById('upChar');
+let valnumber = document.getElementById('number');
+let valspeChar = document.getElementById('speChar');
+let valeigChar = document.getElementById('eigChar');
+
+function passwordvalidation(data) {
+  const lowChar = RegExp('(?=.*[a-z])');
+  const upChar = RegExp('(?=.*[A-Z])');
+  const number = RegExp('(?=.*[0-9])');
+  const speChar = RegExp('(?=.*[!@#$%^&*])');
+
+  if (lowChar.test(data)) {
+    vallowChar.classList.add('valid');
+  } else {
+    vallowChar.classList.remove('valid');
+  }
+
+  if (upChar.test(data)) {
+    valupChar.classList.add('valid');
+  } else {
+    valupChar.classList.remove('valid');
+  }
+
+  if (number.test(data)) {
+    valnumber.classList.add('valid');
+  } else {
+    valnumber.classList.remove('valid');
+  }
+
+  if (speChar.test(data)) {
+    valspeChar.classList.add('valid');
+  } else {
+    valspeChar.classList.remove('valid');
+  }
+
+  if (data.length >= 8) {
+    valeigChar.classList.add('valid');
+  } else {
+    valeigChar.classList.remove('valid');
+  }
+}
+
+// ---- ---- Hide Password ---- ---- //
+showbtn.onclick = function () {
+  if (passwordInput.value.length >= 1) {
+    if (passwordInput.type === 'password') {
+      passwordInput.setAttribute('type', 'text');
+      showbtn.classList.remove('fa-eye');
+      showbtn.classList.add('fa-eye-slash');
+    } else {
+      passwordInput.setAttribute('type', 'password');
+      showbtn.classList.remove('fa-eye-slash');
+      showbtn.classList.add('fa-eye');
+    }
+  }
+};
+
+const emptyUpdate = (
+  inputReference,
+  emptyErrorReference,
+  otherErrorReference
+) => {
+  if (!inputReference.value) {
+    emptyErrorReference.classList.remove('hide');
+    otherErrorReference.classList.add('hide');
+    inputReference.classList.add('error');
+  } else {
+    emptyErrorReference.classList.add('hide');
+  }
+};
+const errorUpdate = (inputReference, errorReference) => {
+  errorReference.classList.remove('hide');
+  inputReference.classList.remove('valid');
+  inputReference.classList.add('error');
+};
+
+const validInput = (inputReference) => {
+  inputReference.classList.remove('error');
+  inputReference.classList.add('valid');
+};
+
+userNameInput.addEventListener('input', () => {
+  if (textVerify(userNameInput.value)) {
+    userNameError.classList.add('hide');
+    validInput(userNameInput);
+  } else {
+    errorUpdate(userNameInput, userNameError);
+    emptyUpdate(userNameInput, userNameEmpty, userNameError);
+  }
+});
+
+emailInput.addEventListener('input', () => {
+  if (emailVerify(emailInput.value)) {
+    emailError.classList.add('hide');
+    validInput(emailInput);
+  } else {
+    errorUpdate(emailInput, emailError);
+    emptyUpdate(emailInput, emailEmpty, emailError);
+  }
+});
+
+phoneInput.addEventListener('input', () => {
+  if (phoneVerify(phoneInput.value)) {
+    phoneError.classList.add('hide');
+    validInput(phoneInput);
+  } else {
+    errorUpdate(phoneInput, phoneError);
+    emptyUpdate(phoneInput, phoneEmpty, phoneError);
+  }
+});
+
+passwordInput.addEventListener('input', () => {
+  if (passwordVerify(passwordInput.value)) {
+    passwordError.classList.add('hide');
+    validInput(passwordInput);
+  } else {
+    errorUpdate(passwordInput, passwordError);
+    emptyUpdate(passwordInput, passwordEmpty, passwordError);
+  }
+});
+
+regiterBtn.addEventListener('click', function (event) {
+  event.preventDefault();
+  if (invalidCls.length == 0 && validCls.length == 9) {
+    showvalPopup();
+  } else {
+    showerrorPopup();
+  }
+});
+
+let Valpopup = document.getElementById('Valpopup');
+let errorpopup = document.getAnimations('errorpopup');
+
+function showvalPopup() {
+  Valpopup.classList.add('open_popup');
+}
+function showerrorPopup() {
+  errorpopup.classList.add('open_popup');
+}
+function closevalPopup() {
+  Valpopup.classList.remove('open_popup');
+  window.location.reload();
+}
+function closevalPopup() {
+  errorpopup.classList.remove('open_popup');
+}
